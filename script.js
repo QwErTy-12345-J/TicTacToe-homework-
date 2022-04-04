b57971864970eda9217f4a27536d93e358543285
QwErTy-12345-J <81107528+QwErTy-12345-J@users.noreply.github.com>
2022-04-04 19:02:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const game = document.querySelector('.game-field');
 const resetButton = document.querySelector('.reset-button');
 const fields = document.querySelectorAll('.field');
 
+
 const playerOne = {name: 'Первый Игрок', symbol: 'x'};
 const playerTwo = {name: 'Второй Игрок', symbol: 'o'};
 const cancelButton = document.querySelector('.cancel-turn');
@@ -43,6 +44,7 @@ const circle = `<svg id="circle" version="1.1" xmlns="http://www.w3.org/2000/svg
                 </g>
             </svg>`;
 
+
 const cross = `<svg id="cross" width="150px" height="150px" viewBox="0 0 15 15" version="1.1"  xmlns="http://www.w3.org/2000/svg">
                 <path d="M2.64,1.27L7.5,6.13l4.84-4.84C12.5114,1.1076,12.7497,1.0029,13,1c0.5523,0,1,0.4477,1,1&#xA;&#9;c0.0047,0.2478-0.093,0.4866-0.27,0.66L8.84,7.5l4.89,4.89c0.1648,0.1612,0.2615,0.3796,0.27,0.61c0,0.5523-0.4477,1-1,1&#xA;&#9;c-0.2577,0.0107-0.508-0.0873-0.69-0.27L7.5,8.87l-4.85,4.85C2.4793,13.8963,2.2453,13.9971,2,14c-0.5523,0-1-0.4477-1-1&#xA;&#9;c-0.0047-0.2478,0.093-0.4866,0.27-0.66L6.16,7.5L1.27,2.61C1.1052,2.4488,1.0085,2.2304,1,2c0-0.5523,0.4477-1,1-1&#xA;&#9;C2.2404,1.0029,2.4701,1.0998,2.64,1.27z"/>
             </svg>`;
@@ -51,10 +53,8 @@ const crossClass = 'x';
 const circleClass = 'o';
 
 let res = document.querySelector('.result');
-
 let step = false;
-let counter = 0;
-
+let	counter = 0;
 let lastField = null;
 
 function drawCross(target) {
@@ -75,7 +75,7 @@ function makeMove(e) {
 
 	if (e.target.innerHTML || !e.target.classList.contains('field') ) return;
 
-	buf = e.target;
+	lastField = e.target;
 	cancelButton.removeAttribute('disabled');
 
 	if (!step) {
